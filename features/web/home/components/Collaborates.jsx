8919a94e65e37bc0aead7c749a13903807ce467c
saifultamim38 <saifultamim38@gmail.com>
2025-06-24 18:10:07 +0600

    ui

--- features/web/home/components/Collaborates.jsx
+++ features/web/home/components/Collaborates.jsx
@@ -5,7 +5,7 @@ const Collaborates = () => {
         <fieldset className="border-2 border-[#EE3373] rounded-lg px-6 relative py-5">
           <legend className="text-xl md:text-3xl font-semibold text-black font-siliguri pl-5 md:pl-8 pr-5 md:pr-6 relative inline-block">
             Collaboration with
-            <span className="absolute top-3 left-0 w-2.5 h-2.5 bg-[#EE3373] rounded-full hidden md:inline-block"></span>
+            <span className="absolute top-3.5 left-0 w-2.5 h-2.5 bg-[#EE3373] rounded-full hidden md:inline-block"></span>
             <span className="absolute top-3 right-0 w-2.5 h-2.5 bg-[#EE3373] rounded-full hidden md:inline-block"></span>
           </legend>
         </fieldset>
